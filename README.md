855a33187bd0f4d890de1a25c65661bd0b6a9114
Thorgal <usa.rs@live.com>
2018-09-10 23:42:22 -0400

    MVP finished, basic functionality developed

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A motivational tool for procrastinators
 
 Inspired by [this TEDTalk](https://www.youtube.com/watch?v=arj7oStGLkU)
+This tool will help you visualize the time you already lived and the estimated life you have left
 
--- script.js
+++ script.js
@@ -1,25 +1,92 @@
 (() => {
     'use strict';
-    const getData = () => {
+    const formatTime = (lifetime) => {
+        const years = Math.floor(lifetime);
+        const extraWeeks = lifetime - years;
+        const weeks = Math.round(extraWeeks * 52);
+        let textOutput = `${years} years`;
+        if (weeks) textOutput += ` ${weeks} weeks`;
+        return textOutput;
+    };
+
+    const drawChart = (lifetime, timePassed) => {
+        const chartDiv = document.querySelector('div.chart');
+        chartDiv.innerHTML = '';
+        const weeks = Math.round(lifetime * 52);
+        const pastWeeks = Math.round(timePassed * 52);
+        console.log(`weeks: ${weeks}`);
+        for (let i = 0; i < weeks; i++){
+            
+            const weekDiv = document.createElement('div');
+            weekDiv.classList.add('weekDiv');
+            if (i < pastWeeks) weekDiv.classList.add('passed');
+            chartDiv.appendChild(weekDiv);
+        }
+
+    };
+
+    const readData = (year, gender) => {
+        //values
+        let yearVal = year.options[year.selectedIndex].value;
+        let genderVal = gender.options[gender.selectedIndex].value;
+        console.log(yearVal);
+        console.log(genderVal);
+        if (genderVal === 'idiot') {
+            openModal('idiot');
+            return;
+        }
+        calculate(yearVal, genderVal);
+    };
+
+    const calculate = (year, gender) => {
         console.log('fetch started');
         const output = document.querySelector('.output');
-        const gender = 'SP.DYN.LE00.IN'; //general
-        const male = `SP.DYN.LE00.MA.IN`;
-        const female = `SP.DYN.LE00.FE.IN`;
+        const lifetime = document.querySelector('.lifetime');
+        const lifeLeft = document.querySelector('.lifeLeft');
+        const genderStr = 'SP.DYN.LE00.IN'; //both genders, unused
+        const maleStr = `SP.DYN.LE00.MA.IN`;
+        const femaleStr = `SP.DYN.LE00.FE.IN`;
 
         // const gender = 'SPODYNLE00MAIN';
-        const variableURL = `countries/POL/indicators/${female}`;
-        const rootURL = `https://api.worldbank.org/v2/${variableURL}?date=1960:2007&format=json`;
+        // const variableURL = `${female}`;
+        const rootURL = `https://api.worldbank.org/v2/countries/POL/indicators/${gender === 'man' ? maleStr : femaleStr}?date=${year}:${year}&format=json`;
+        console.log(`rootUrl: ${rootURL}`);
         fetch(rootURL).then((res) => res.json(), (err) => console.error(err)).then((json) => {
             console.log('final step');
             console.log(json);
+            const life = json[1][0].value;
+            
             output.textContent = JSON.stringify(json);
+            
+            lifetime.textContent = formatTime(life);
+            const today = new Date().getFullYear();
+            const timePassed = today - year;
+            console.log(timePassed);
+            lifeLeft.textContent = formatTime(life - timePassed);
+            drawChart(life, timePassed);
         });
     };
+
     const init = () => {
         console.log('app started');
-        const dataBtn = document.querySelector('button.data');
-        dataBtn.addEventListener('click', getData, false);
+        
+        //references
+        const yearBox = document.querySelector('#year');
+        const startBtn = document.querySelector('.submit');
+        const gender = document.querySelector('#gender');
+        //populate website
+        for (let i = 2016; i > 1959; i--){
+            const option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.textContent = i;
+            yearBox.appendChild(option);
+        }
+
+        startBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            readData(year, gender);
+            
+        });
     };
 
     document.addEventListener('DOMContentLoaded', init, false);
